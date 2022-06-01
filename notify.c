9a68a8d13a6dc41878439cd8dbeb5e57ea7ba824
qStanL <stanl2062@gmail.com>
2022-06-02 00:58:21 +0300

    by local git

---
+++ notify.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include "libpq-fe.h"
+
+int main(int argc, char **argv) {
+        const char *conninfo;
+        PGconn     *conn;
+        PGresult   *res;
+        PGnotify   *notify;
+
+        char db_name[] = "saulyak_vladislav";
+        char user_name[] = "saulyak_vladislav";
+
+        conn = PQsetdbLogin(NULL,NULL,NULL,NULL,db_name,user_name,NULL);
+        if (PQstatus(conn) != CONNECTION_OK) {
+                fprintf(stderr, "Connection to database failed: %s", PQerrorMessage(conn));
+        PQfinish(conn);
+        exit(1);
+        }
+
+        res = PQexec(conn, "NOTIFY saulyak_vladislav");
+        if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+                fprintf(stderr, "NOTIFY command failed: %s", PQerrorMessage(conn));
+                PQclear(res);
+                PQfinish(conn);
+                exit(1);
+        }
+        PQclear(res);
+        PQfinish(conn);
+        return 0;
+}
